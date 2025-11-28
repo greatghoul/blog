74a0841a48ed7caf6ff94190147a2c49f6e6b816
greatghoul <greatghoul@gmail.com>
2025-11-28 12:47:29 +0800

    改进 publish_issue.js 脚本和添加精选页面

--- .github/workflows/publish_issue.js
+++ .github/workflows/publish_issue.js
@@ -1,7 +1,23 @@
 const matter = require("gray-matter")
 const fs = require("fs");
 
-const ALLOWED_MILESTONES = ['post', 'page'];
+const ISSUE_TYPES = [
+  {
+    milestone: '文章',
+    layout: 'post',
+    folder: '_posts'
+  },
+  {
+    milestone: '页面',
+    layout: 'page',
+    folder: '.'
+  },
+  {
+    milestone: '精选',
+    layout: 'page',
+    folder: 'picks'
+  }
+];
 
 const issue = JSON.parse(process.argv[2]);
 const { data, content } = matter(issue.body);
@@ -13,36 +29,35 @@ if (!milestone) {
   process.exit(0);
 }
 
-if (!ALLOWED_MILESTONES.includes(milestone.title)) {
-  console.warn(`Skip publishing issue ${issue.number} because milestone ${milestone.title} is not supported. only ${ALLOWED_MILESTONES.join(',')} are allowed.`);
+const issueType = ISSUE_TYPES.find(type => type.milestone === milestone.title);
+if (!issueType) {
+  console.warn(`Skip publishing issue ${issue.number} because milestone ${milestone.title} is not supported. only ${ISSUE_TYPES.map(type => type.milestone).join(',')} are allowed.`);
   process.exit(0);
 }
 
-// use milestone title as layout
-data.layout = milestone.title
+// use mapped milestone as layout
+data.layout = issueType.layout
+
 data.title = issue.title
 data.tags = issue.labels.map(x => x.name)
 data.issue = issue.number
+
+// set date for all types, format: YYYY-MM-DD
+const date = new Date(data.date || issue.created_at); 
+const dateOptions = { timeZone: "Asia/Shanghai", year: "numeric", month: "2-digit", day: "2-digit" };
+data.date = date.toLocaleString("zh-CN", dateOptions).replace(/\//g, "-");
 
 let filename = '';
 if (data.layout === 'post') {
   // post always use issue number as slug prefix
   data.slug = data.slug ? `${data.issue}-${data.slug}` : `${data.issue}`;
-
-  // if date not set, use issue created_at
-  // date will be formatted as YYYY-MM-DD and use as post filename prefix.
-  const date = new Date(data.date || issue.created_at); 
-  const dateOptions = { timeZone: "Asia/Shanghai", year: "numeric", month: "2-digit", day: "2-digit" };
-  data.date = date.toLocaleString("zh-CN", dateOptions).replace(/\//g, "-");
-
-  filename = `_posts/${data.date}-${data.slug}.md`;
+  filename = `${issueType.folder}/${data.date}-${data.slug}.md`;
 } else {
   // page use issue number as slug not provided.
   // page slug will be used as filename.  <slug>.md
   // to override page link, use permalink in frontmatter.
   data.slug = data.slug || `${data.issue}`;
-
-  filename = `${data.slug}.md`;
+  filename = `${issueType.folder}/${data.slug}.md`;
 }
 
 const filebody = matter.stringify(content, data)
